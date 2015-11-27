be2f5b3ad034ea50e86803104ed938087b670f09
Kevindeving <98kevchen@gmail.com>
2015-11-27 21:12:30 +0100

    geehrc: small updates

--- device.mk
+++ device.mk
@@ -274,6 +274,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.qualcomm.sensors.smd=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
+        dalvik.vm.dex2oat-filter=speed \
         dalvik.vm.dex2oat-swap=false
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
